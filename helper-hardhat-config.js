e25c47fec1b9e6c9fccfdf23f80db7a3ab4c8683
root <root@S>
2023-09-17 21:38:27 +0800

    VERSION1.0

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -2,17 +2,11 @@ const { ethers } = require("hardhat")
 
 const networkConfig = {
     11155111: {},
-    31337: {
-        name: "localhost",
-        wethToken: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
-        lendingPoolAddressesProvider: "0xb53c1a33016b2dc2ff3653530bff1848a515c8c5",
-        daiEthPriceFeed: "0x773616E4d11A78F511299002da57A0a94577F1f4",
-        daiToken: "0x6b175474e89094c44da98b954eedeac495271d0f"
-    }
+    31337: {},
+    80001: {}
 }
 
-const developmentChains = ["hardhat", "localhost"]
-const VERIFICATION_BLOCK_CONFIRMATIONS = 6
+
 module.exports = {
     networkConfig
 }
